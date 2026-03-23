a35d1b2c05b1ecda095270d79927906956330ce4
Harsahib Singh <s.harsahib2007@gmail.com>
2026-03-23 09:49:15 +0530

    Integrate Google OAuth client ID from config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GOOGLE_CLIENT_ID, hasGoogleClientId } from "./lib/config";
+
 
 export default function App() {
+  if (!hasGoogleClientId()) {
+    return <RouterProvider router={router} />;
+  }
+
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          <RouterProvider router={router} />
     </GoogleOAuthProvider>
   );
 }
